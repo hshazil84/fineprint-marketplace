08e6f56cfaa9eea6ab6d9d424ed0f974492750bf
hshazil84 <hshazil@gmail.com>
2026-04-19 00:24:50 +0500

    Update page.tsx

--- app/artwork/[id]/page.tsx
+++ app/artwork/[id]/page.tsx
@@ -94,7 +94,6 @@ export default function ArtworkPage() {
                     display: 'block',
                     pointerEvents: 'none',
                     userSelect: 'none',
-                    WebkitUserDrag: 'none',
                     objectFit: 'contain',
                     maxHeight: 520,
                   }}
