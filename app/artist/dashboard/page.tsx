dc3ecae92fc3923454351551d6a4ebf22fea4029
hshazil84 <hshazil@gmail.com>
2026-04-20 15:42:13 +0500

    Update page.tsx

--- app/artist/dashboard/page.tsx
+++ app/artist/dashboard/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import Header from '@/app/components/Header'
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation'
 import { createClient } from '@/lib/supabase'
